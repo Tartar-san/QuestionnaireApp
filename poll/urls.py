0385343318b4fe92972ac18df71a553e9ec6e443
Tartar-san <secondmailgreen@gmail.com>
2017-02-01 15:12:46 +0200

    Hotfix for ulrpatterns

--- poll/urls.py
+++ poll/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.get_initial_page, name='initial_page'),
-    url(r'^question$', views.get_page, name='main'),
+    url(r'^question/?$', views.get_page, name='main'),
     url(r'^next_page$', views.post_answer, name='next'),
 ]
