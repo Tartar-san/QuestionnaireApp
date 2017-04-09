c8ec7834e8f5f46244560d5b64a5fcc84f3f6f34
MNITD <bogdanleblamk@ukr.net>
2017-04-09 12:31:03 +0300

    added unknown coin

--- static/poll/scripts/lottery.js
+++ static/poll/scripts/lottery.js
@@ -1,13 +1,46 @@
+function hideUnknown(id) {
+
+    setHandler(id, toggleCoin);
+}
+function toggleCoin(id) {
+
+
+}
+
+
 function setHandler(id) {
     $('#' + id + '_checkbox').on('change', function () {
+
+        if (!checkUnknown(id)) {
+            $('#' + id + '_checkbox')[0].checked = false;
+            $('#' + id + '_unknown').hide();
+            $('#' + id + '_tail').show();
+        } else {
             $('#' + id + '_tail').toggle();
             $('#' + id + '_head').toggle();
+        }
     });
+}
+
+function checkUnknown(id) {
+    return $('#' + id + '_unknown').css('display') === 'none';
+}
+
+function checkCoins() {
+    for (var i = 1; i < 11; i++) {
+        if (!checkUnknown('coin' + i)) return false;
+    }
+    return true;
 }
 
 $(document).ready(function () {
     for (var i = 1; i < 11; i++) {
         setHandler('coin' + i);
     }
+
+    $('#mainForm').submit(function () {
+        if (!checkCoins()) return false;
+
+    });
 
 });
