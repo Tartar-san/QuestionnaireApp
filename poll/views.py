46756594685afa583845d2c966b787c6abc85ab4
Tartar-san <secondmailgreen@gmail.com>
2017-05-01 23:16:10 +0300

    export all every time

--- poll/views.py
+++ poll/views.py
@@ -17,6 +17,7 @@ import csv
 spreadsheet_updater = SpreadSheetUpdater(filename=os.path.join(settings.STATICFILES_DIRS[0], 'poll/client_secret.json'))
 
 def csv_download(request):
+    os.system("python /home/sociology/QuestionnaireApp/export_all.py")
     wrapper = open(os.path.join(settings.STATICFILES_DIRS[0], 'poll/poll.csv'), 'r' , encoding='cp1251')
     response = HttpResponse(content_type='text/csv; charset=windows-1251')
     response['Content-Disposition'] = "attachment; filename=results_of_poll.csv"
