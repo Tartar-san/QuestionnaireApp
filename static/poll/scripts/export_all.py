0e9ca1f6cc73329563f16b130cc212e4ca1834c3
dobosevych <dobosevych@gmail.com>
2017-05-04 19:01:43 +0300

    Excel export fixes

--- static/poll/scripts/export_all.py
+++ static/poll/scripts/export_all.py
@@ -2,7 +2,7 @@
 import sqlite3
 import csv
 import time
-import pandas as pb
+#import pandas as pb
 
 def export():
 
@@ -37,12 +37,13 @@ def export():
     conn.commit()
     conn.close()
 
-    df = pb.read_csv('/home/sociology/QuestionnaireApp/static/poll/poll.csv', encoding="cp1251")
-    writer = pb.ExcelWriter('/home/sociology/QuestionnaireApp/static/poll/poll.xlsx')
-    df.to_excel(writer,'Poll')
-    writer.save()
+    # df = pb.read_csv('/home/sociology/QuestionnaireApp/static/poll/poll.csv', encoding="cp1251")
+    # writer = pb.ExcelWriter('/home/sociology/QuestionnaireApp/static/poll/poll.xlsx')
+    # df.to_excel(writer,'Poll')
+    # writer.save()
 
-
+if __name__ == "__main__":
+    export()
 
 
 
